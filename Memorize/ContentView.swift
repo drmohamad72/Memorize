5e9d00fb377545b59b092bee49aa095eaccb4c87
mohamad tqdc <m.taghadosi72@gmail.com>
2024-01-07 11:55:57 +0330

    it is my

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -4,21 +4,65 @@
 //
 //  Created by mohamad on 1/7/24.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        
+        HStack(){
+            
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+            
+            
+           
         }
-        .padding()
+        
+        .foregroundColor(.orange)
+        .imageScale(.small)
     }
+    
 }
 
 #Preview {
     ContentView()
 }
+struct CardView: View{
+    var isFaceUp : Bool = false
+    
+    var body : some View{
+        ZStack(content:
+                {
+            
+            
+            if isFaceUp{
+                
+                
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2))
+                
+                Text("👻").padding()
+                    .font(.largeTitle)
+            }
+            
+            else{
+                RoundedRectangle(cornerRadius: 12)
+                
+                RoundedRectangle(cornerRadius: 12)
+                
+                    .strokeBorder(style: StrokeStyle(lineWidth: 2))
+            }
+        })
+    }
+    
+}
+                    
+     
