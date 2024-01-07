f72ff979344e388718816e65f3ec1ccc0b321e2b
mohamad tqdc <m.taghadosi72@gmail.com>
2024-01-07 11:55:14 +0330

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by mohamad on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
